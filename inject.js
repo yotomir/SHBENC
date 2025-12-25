44b92de5683e942ed5f15123a7a5f0ee925ad013
Yotomir <yotomir.karagiozov@gmail.com>
2025-12-25 11:50:54 +0200

    Refactor hidden blocks category injection
    Refactor hidden blocks setup and injection logic.

--- inject.js
+++ inject.js
@@ -1,46 +1,38 @@
 (function() {
-    function setupHiddenBlocks() {
-        if (!window.ScratchBlocks) return;
-        const Blocks = window.ScratchBlocks;
+    const toolboxXML = `
+        <category name="Hidden Blocks" id="hidden_cat" colour="#ff0000" secondaryColour="#cc0000">
+            <block type="event_whentouchingobject">
+                <value name="TOUCHINGOBJECTMENU"><shadow type="event_touchingobjectmenu"/></value>
+            </block>
+            <block type="control_for_each">
+                <value name="VALUE"><shadow type="math_whole_number"><field name="NUM">10</field></shadow></value>
+            </block>
+            <block type="control_while"/>
+            <block type="control_incr_counter"/>
+            <block type="control_clear_counter"/>
+            <block type="control_get_counter"/>
+            <block type="operator_mathop">
+                <field name="OPERATOR">e ^</field>
+                <value name="NUM"><shadow type="math_number"><field name="NUM">10</field></shadow></value>
+            </block>
+        </category>`;
 
-        // Дефинираме категорията с цвят #ff0000 и икона-коте в името
-        const toolboxXML = `
-            <category name="Hidden Blocks" id="hidden_cat" colour="#ff0000" secondaryColour="#cc0000">
-                <block type="event_whentouchingobject">
-                    <value name="TOUCHINGOBJECTMENU"><shadow type="event_touchingobjectmenu"/></value>
-                </block>
-                <block type="control_for_each">
-                    <value name="VALUE"><shadow type="math_whole_number"><field name="NUM">10</field></shadow></value>
-                </block>
-                <block type="control_while"/>
-                <block type="control_incr_counter"/>
-                <block type="control_clear_counter"/>
-                <block type="control_get_counter"/>
-                <block type="operator_mathop">
-                    <field name="OPERATOR">e ^</field>
-                    <value name="NUM"><shadow type="math_number"><field name="NUM">10</field></shadow></value>
-                </block>
-            </category>`;
-
-        const updateToolbox = () => {
-            const workspace = Blocks.mainWorkspace;
-            if (workspace && workspace.getFlyout()) {
+    function injectCategory() {
+        if (window.ScratchBlocks && window.ScratchBlocks.mainWorkspace) {
+            const workspace = window.ScratchBlocks.mainWorkspace;
+            const flyout = workspace.getFlyout();
+            
+            if (flyout) {
                 let currentXML = workspace.options.languageTree.innerHTML;
-                // Проверяваме дали вече не е добавена, за да не се повтаря
                 if (!currentXML.includes('id="hidden_cat"')) {
                     workspace.updateToolbox(currentXML + toolboxXML);
+                    console.log("УСПЕХ: Категорията е добавена!");
                 }
             }
-        };
-        
-        updateToolbox();
+        }
     }
 
-    // Изчакваме малко повече (3 секунди), за да зареди Scratch напълно
-    const initInterval = setInterval(() => {
-        if (window.vm && window.ScratchBlocks && window.ScratchBlocks.mainWorkspace) {
-            clearInterval(initInterval);
-            setupHiddenBlocks();
-        }
-    }, 3000);
+    // Този таймер проверява на всеки 2 секунди дали категорията е там
+    // Ако я няма - я добавя автоматично.
+    setInterval(injectCategory, 2000);
 })();
