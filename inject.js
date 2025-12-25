5399d46743db28fb6c2d103abcb92a3c0541080f
Yotomir <yotomir.karagiozov@gmail.com>
2025-12-25 12:07:44 +0200

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,28 +1,31 @@
 (function() {
     const toolboxXML = `
         <category name="Hidden Blocks" id="hidden_cat" colour="#ff0000" secondaryColour="#cc0000">
-            <block type="event_whentouchingobject">
-                <value name="TOUCHINGOBJECTMENU"><shadow type="event_touchingobjectmenu"/></value>
-            </block>
+            <block type="control_while"/>
             <block type="control_for_each">
                 <value name="VALUE"><shadow type="math_whole_number"><field name="NUM">10</field></shadow></value>
             </block>
-            <block type="control_while"/>
+            <block type="control_get_counter"/>
             <block type="control_incr_counter"/>
             <block type="control_clear_counter"/>
-            <block type="control_get_counter"/>
         </category>`;
 
-    function injectCategory() {
-        if (window.ScratchBlocks && window.ScratchBlocks.mainWorkspace) {
-            const workspace = window.ScratchBlocks.mainWorkspace;
-            if (workspace.getFlyout()) {
-                let currentXML = workspace.options.languageTree.innerHTML;
-                if (!currentXML.includes('id="hidden_cat"')) {
-                    workspace.updateToolbox(currentXML + toolboxXML);
+    function tryInject() {
+        try {
+            if (window.ScratchBlocks && window.ScratchBlocks.mainWorkspace) {
+                const ws = window.ScratchBlocks.mainWorkspace;
+                // Проверка дали категорията вече е там
+                if (!ws.options.languageTree.innerHTML.includes('id="hidden_cat"')) {
+                    const currentXML = ws.options.languageTree.innerHTML;
+                    ws.updateToolbox(currentXML + toolboxXML);
+                    console.log("HIDDEN BLOCKS CATEGORY DEPLOYED!");
                 }
             }
+        } catch (e) {
+            // Ако има грешка, просто изчакваме следващия цикъл
         }
     }
-    setInterval(injectCategory, 2000);
+
+    // Проверяваме на всяка секунда, защото ScratchAddons може да презареди менюто
+    setInterval(tryInject, 1000);
 })();
