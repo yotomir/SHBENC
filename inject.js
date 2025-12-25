0f79236f1ac180246531f57985b2174db3e832a9
Yotomir <yotomir.karagiozov@gmail.com>
2025-12-25 11:37:18 +0200

    Update inject.js

--- inject.js
+++ inject.js
@@ -3,27 +3,23 @@
         if (!window.ScratchBlocks) return;
         const Blocks = window.ScratchBlocks;
 
-        // Дефинираме червената категория с всички скрити блокове + БОНУС
+        // Дефинираме червената категория (#ff0000)
         const toolboxXML = `
             <category name="Hidden Blocks" id="hidden_cat" colour="#ff0000" secondaryColour="#cc0000">
                 <block type="event_whentouchingobject">
                     <value name="TOUCHINGOBJECTMENU"><shadow type="event_touchingobjectmenu"/></value>
                 </block>
-
                 <block type="control_for_each">
                     <value name="VALUE"><shadow type="math_whole_number"><field name="NUM">10</field></shadow></value>
                 </block>
                 <block type="control_while"/>
-                
                 <block type="control_incr_counter"/>
                 <block type="control_clear_counter"/>
                 <block type="control_get_counter"/>
-
                 <block type="operator_mathop">
                     <field name="OPERATOR">e ^</field>
                     <value name="NUM"><shadow type="math_number"><field name="NUM">10</field></shadow></value>
                 </block>
-
                 <block type="operator_round">
                     <value name="NUM"><shadow type="math_number"><field name="NUM">60</field></shadow></value>
                 </block>
@@ -35,18 +31,19 @@
                 let currentXML = workspace.options.languageTree.innerHTML;
                 if (!currentXML.includes('id="hidden_cat"')) {
                     workspace.updateToolbox(currentXML + toolboxXML);
+                    console.log("Category added successfully!");
                 }
             }
         };
-
+        
         updateToolbox();
     }
 
+    // Изчакваме 2 секунди, за да зареди Scratch напълно
     const initInterval = setInterval(() => {
         if (window.vm && window.ScratchBlocks && window.ScratchBlocks.mainWorkspace) {
             clearInterval(initInterval);
             setupHiddenBlocks();
-            console.log("Pro Mode Enabled: Red Category & Bonus Blocks Ready! 🔴🚀");
         }
-    }, 1000);
+    }, 2000);
 })();
