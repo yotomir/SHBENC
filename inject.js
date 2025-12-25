2e620ba73478653f18650f0542d446ac07e4a336
Yotomir <yotomir.karagiozov@gmail.com>
2025-12-25 12:21:23 +0200

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,31 +1,15 @@
 (function() {
-    const toolboxXML = `
-        <category name="Hidden Blocks" id="hidden_cat" colour="#ff0000" secondaryColour="#cc0000">
-            <block type="control_while"/>
-            <block type="control_for_each">
-                <value name="VALUE"><shadow type="math_whole_number"><field name="NUM">10</field></shadow></value>
-            </block>
-            <block type="control_get_counter"/>
-            <block type="control_incr_counter"/>
-            <block type="control_clear_counter"/>
-        </category>`;
-
-    function tryInject() {
-        try {
-            if (window.ScratchBlocks && window.ScratchBlocks.mainWorkspace) {
-                const ws = window.ScratchBlocks.mainWorkspace;
-                // Проверка дали категорията вече е там
-                if (!ws.options.languageTree.innerHTML.includes('id="hidden_cat"')) {
-                    const currentXML = ws.options.languageTree.innerHTML;
-                    ws.updateToolbox(currentXML + toolboxXML);
-                    console.log("HIDDEN BLOCKS CATEGORY DEPLOYED!");
-                }
+    const toolboxXML = '<category name="Hidden Blocks" id="hidden_cat" colour="#ff0000" secondaryColour="#cc0000"><block type="control_while"/><block type="control_for_each"><value name="VALUE"><shadow type="math_whole_number"><field name="NUM">10</field></shadow></value></block><block type="control_get_counter"/><block type="control_incr_counter"/><block type="control_clear_counter"/></category>';
+    
+    function inject() {
+        if (window.ScratchBlocks && window.ScratchBlocks.mainWorkspace) {
+            const ws = window.ScratchBlocks.mainWorkspace;
+            const tree = ws.options.languageTree.innerHTML;
+            if (!tree.includes('id="hidden_cat"')) {
+                ws.updateToolbox(tree + toolboxXML);
+                console.log("Activated!");
             }
-        } catch (e) {
-            // Ако има грешка, просто изчакваме следващия цикъл
         }
     }
-
-    // Проверяваме на всяка секунда, защото ScratchAddons може да презареди менюто
-    setInterval(tryInject, 1000);
+    setInterval(inject, 2000);
 })();
