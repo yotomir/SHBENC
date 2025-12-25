06b0aaec2497f8fc91daf7632cb21209e840f403
Yotomir <yotomir.karagiozov@gmail.com>
2025-12-25 11:26:24 +0200

    Enhance hidden blocks setup with bonus category

--- inject.js
+++ inject.js
@@ -3,19 +3,27 @@
         if (!window.ScratchBlocks) return;
         const Blocks = window.ScratchBlocks;
 
-        // XML за категорията - цвят #ff0000 (червено)
+        // Дефинираме червената категория с всички скрити блокове + БОНУС
         const toolboxXML = `
             <category name="Hidden Blocks" id="hidden_cat" colour="#ff0000" secondaryColour="#cc0000">
                 <block type="event_whentouchingobject">
                     <value name="TOUCHINGOBJECTMENU"><shadow type="event_touchingobjectmenu"/></value>
                 </block>
+
                 <block type="control_for_each">
                     <value name="VALUE"><shadow type="math_whole_number"><field name="NUM">10</field></shadow></value>
                 </block>
                 <block type="control_while"/>
+                
                 <block type="control_incr_counter"/>
                 <block type="control_clear_counter"/>
                 <block type="control_get_counter"/>
+
+                <block type="operator_mathop">
+                    <field name="OPERATOR">e ^</field>
+                    <value name="NUM"><shadow type="math_number"><field name="NUM">10</field></shadow></value>
+                </block>
+
                 <block type="operator_round">
                     <value name="NUM"><shadow type="math_number"><field name="NUM">60</field></shadow></value>
                 </block>
@@ -34,12 +42,11 @@
         updateToolbox();
     }
 
-    // Търсене на работната среда на Scratch
     const initInterval = setInterval(() => {
         if (window.vm && window.ScratchBlocks && window.ScratchBlocks.mainWorkspace) {
             clearInterval(initInterval);
             setupHiddenBlocks();
-            console.log("Hidden Blocks Unlocked in Red! 🔴");
+            console.log("Pro Mode Enabled: Red Category & Bonus Blocks Ready! 🔴🚀");
         }
     }, 1000);
 })();
